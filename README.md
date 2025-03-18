844ce0c87272e5f8d36b57cbe57da7d51d3769b6
Roger Tang <rogertangdq@gmail.com>
2025-03-18 16:23:08 -0700

    add cosmos db to local dev

---
+++ README.md
@@ -0,0 +1,3 @@
+## How to start up dependency services?
+ - At project root dir, run `docker compose up -d`
+ - Verify the service at http://localhost:8081/
--- build.gradle.kts
+++ build.gradle.kts
@@ -38,15 +38,18 @@ dependencies {
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
 	//implementation("org.springframework.cloud:spring-cloud-function-web")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	annotationProcessor("org.projectlombok:lombok")
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
